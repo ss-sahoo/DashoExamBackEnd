c0ae7a6e4fb537274ce3a8af09a483efabcfa16d
tap222 <mtapas.mohanty95@gmail.com>
2026-03-12 12:59:03 +0530

    added chnages

--- locustfile.py
+++ locustfile.py
@@ -1,16 +1,15 @@
 from locust import HttpUser, task, between
 
 class ExamUser(HttpUser):
-    wait_time = between(1, 3)
+    wait_time = between(1, 2)
+
+    def on_start(self):
+        self.headers = {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc3Mzg5MDI4NSwiaWF0IjoxNzczMjg1NDg1LCJqdGkiOiI0NGRjNjJmYjY4MTM0YmVhYTRjMmVlZWFkOGIzYTRkYSIsInVzZXJfaWQiOjEsImRldmljZV9maW5nZXJwcmludCI6ImZiZWU2YTJmNjdlNTdmZGY0YjRhNWM5MTMyMmNiMGM5Mjk1MTIwMDg5ZmQ2ZTIyOWM5ZGRjMjMyZWUwNDQ4ODMifQ.X69qEBMP8_PHQP_1uXX2x4hEl4KyC9xGCR8lqd8l8_8"
+        }
 
     @task
-    def get_exams(self):
-        self.client.get("/api/exams/")
-
-    @task
-    def get_patterns(self):
-        self.client.get("/api/patterns/")
-
-    @task
-    def admin_page(self):
-        self.client.get("/admin/")
+    def exams(self):
+        with self.client.get("api/exams/exams/", headers=self.headers, catch_response=True) as response:
+            if response.status_code != 200:
+                response.failure(f"Failed with {response.status_code}")
