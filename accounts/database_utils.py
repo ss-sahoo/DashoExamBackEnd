8b1cc32b3022edf92c1e592072c0b01a663b0a8a
ss-sahoo <shradhamilu160@gmail.com>
2026-03-16 15:28:02 +0530

    chnages

--- accounts/database_utils.py
+++ accounts/database_utils.py
@@ -14,7 +14,7 @@ def register_institute_database(institute):
     
     if db_key not in settings.DATABASES:
         default_db = settings.DATABASES['default']
-        # Create the configuration, inheriting all options from default DB
+        # Only include keys that are universally supported across Django versions
         db_config = {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': institute.db_name,
@@ -22,12 +22,6 @@ def register_institute_database(institute):
             'PASSWORD': institute.db_password or default_db.get('PASSWORD'),
             'HOST': institute.db_host or default_db.get('HOST'),
             'PORT': institute.db_port or default_db.get('PORT'),
-            'TIME_ZONE': default_db.get('TIME_ZONE', None),
-            'CONN_MAX_AGE': default_db.get('CONN_MAX_AGE', 0),
-            'OPTIONS': default_db.get('OPTIONS', {}),
-            'ATOMIC_REQUESTS': default_db.get('ATOMIC_REQUESTS', False),
-            'AUTOCOMMIT': default_db.get('AUTOCOMMIT', True),
-            'TEST': default_db.get('TEST', {}),
         }
         
         # Inject into settings (this is not standard but works for dynamic routing)
